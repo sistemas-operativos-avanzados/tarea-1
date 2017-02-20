f45aa5658a102dae8913bce4893c9a0db95a7146
Martin Flores <martin.flores@vitalize.com>
2017-02-19 21:27:17 -0600

    log y clean up

--- src/main.c
+++ src/main.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <stdarg.h>
 
 #define printable(ch) (isprint((unsigned char) ch) ? ch : '#')
 
+int enableLog = 0;
 
 static void usageError(char *progName, char *msg, int opt) {
     if (msg != NULL && opt != 0) {
@@ -12,6 +14,41 @@ static void usageError(char *progName, char *msg, int opt) {
     }
     fprintf(stderr, "Uso: %s [-v prog] [-V prog]\n", progName);
     exit(EXIT_FAILURE);
+}
+
+void logMsg(char *fmt, ...){
+
+    if(enableLog) {
+        va_list ap;
+        char *p, *sval;
+        int ival;
+        va_start(ap, fmt);
+
+        for (p = fmt; *p; p++) {
+            if (*p != '%') {
+                putchar(*p);
+                continue;
+            }
+            switch (*++p) {
+
+                case 'd':
+                    ival = va_arg(ap, int);
+                    printf("%d", ival);
+                    break;
+                case 's':
+                    for (sval = va_arg(ap, char *); *sval; sval++) {
+                        putchar(*sval);
+                    }
+                    break;
+                default:
+                    putchar(*p);
+                    break;
+            }
+        }
+
+        va_end(ap);
+        printf("\n");
+    }
 }
 
 void traceCall(int argc, char *argv[]){
@@ -23,11 +60,15 @@ void traceCall(int argc, char *argv[]){
     char *arguments[argumentsSize];
 
     arguments[0] = "strace"; arguments[1] = "-c"; arguments[2] = "-f";
-//    printf("Arg count = %d, i = %d, ArgSize = %d \n", argc, i, argumentsSize);
+    logMsg("Arg count = %d, i = %d, ArgSize = %d \n", argc, i, argumentsSize);
 
     while(i < argc){
+        //TODO: evitar argumentos -v, -d y -V
+//        if(*argv[i] == 'd' || *argv[i] == 'v' || *argv[i] == 'V'){
+//            continue;
+//        }
         arguments[argumentsIndex] = argv[i];
-//        printf("AA[%d] %s \n", argumentsIndex, arguments[argumentsIndex]);
+        logMsg("AA[%d] %s \n", argumentsIndex, arguments[argumentsIndex]);
         argumentsIndex++;
         i++;
     }
@@ -39,19 +80,22 @@ void traceCall(int argc, char *argv[]){
 
 int main(int argc, char *argv[]) {
 
+
     int opt;
-    while((opt = getopt(argc, argv, "-v:V:")) != EOF) {
-        printf("opt =%3d (%c); optind = %d ", opt, printable(opt), optind);
-        if (opt == '?' || opt == ':') {
-            printf("; optopt =%3d (%c)", optopt, printable(optopt));
-        }
-        printf("\n");
+    while((opt = getopt(argc, argv, "-d-v:V")) != EOF) {
+
 
         switch (opt) {
+            case 'd':
+                enableLog = 1;
+                logMsg("Log habilitado");
+                break;
             case 'v':
+                logMsg("-> Llamando a traceCall con -v");
                 traceCall(argc, argv);
                 break;
             case 'V':
+                logMsg("-> Llamando a traceCall con -V");
                 puts("Presione cualquier tecla para continuar...");
                 getchar();
                 traceCall(argc, argv);
@@ -61,12 +105,15 @@ int main(int argc, char *argv[]) {
             case '?':
                 usageError(argv[0], "Opcion invalida", optopt);
             default:
-                exit(EXIT_FAILURE);
+                usageError(argv[0], "Falta argumento", optopt);
+        }
+
+        logMsg("opt =%3d (%c); optind = %d", opt, printable(opt), optind);
+        if (opt == '?' || opt == ':') {
+            logMsg("; optopt =%3d (%c)", optopt, printable(optopt));
         }
     }
 
 
     return 0;
 }
-
-
